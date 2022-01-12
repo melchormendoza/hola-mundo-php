b397b61ca8244bab6a04c016ca86765b3f1adeae
Melchor Mendoza <melchormendoza@MacBook.local>
2022-01-11 18:02:00 -0600

    implementacion de POO y MVC

---
+++ app/controllers/hola-mundo_controller.php
@@ -0,0 +1,5 @@
+<?php
+require_once("./app/models/hola-mundo_model.php");
+$saludo = new Saludo();
+require_once("./app/views/hola-mundo_view.php");
+?>
---
+++ app/models/hola-mundo_model.php
@@ -0,0 +1,9 @@
+<?php
+class Saludo {
+
+    function holamundo(){
+        $texto = "Hola mundo";
+        return $texto;
+    }
+}
+?>
